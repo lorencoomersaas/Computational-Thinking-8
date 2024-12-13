0e047df00894a5763227bc8514aa718718e01357
lorencoomersaas <lorencoomer@seattleacademy.org>
2024-12-13 22:54:53 +0000

    grandma

---
+++ Projects/grandma.py
@@ -0,0 +1,9 @@
+while True:
+    word = input("whet do you think grandma likes?")
+
+    if "aa" in word or "ee" in word or "ii" in word or "oo" in word or "uu"in word:
+        print(f"grandma doesn't like {word}!")
+    else:
+        print(f"grandma likes {word}!")
+    
+    print("")
