0176324fb0271c01e5806ab7636b6a27840e5820
lorencoomersaas <lorencoomer@seattleacademy.org>
2025-01-10 01:38:05 +0000

    e

--- Projects/project3.py
+++ Projects/project3.py
@@ -1,2 +1,25 @@
-# test
-# test
+# importing libraries
+import turtle as t
+import random as r
+
+# setting speed to 10
+t.speed(10)
+
+# setting bg
+t.Screen().bgcolor('#EBD3F8')
+
+# colors = ['red', 'blue', 'purple']
+colors = ['#AD49E1', '#7A1CAC', '#2E073F']
+# angles = [0, 72, 144, 216, 288]
+angles = [0, 60, 120, 180, 240, 300]
+# angles = [0, 60, 120, 180, 240, 300]
+# angles = [0, 45, 90, 135, 180, 225, 270, 315]
+
+# for loop
+for i in range(99999999999999999):
+    t.color(colors[i%3])
+    t.forward(10)
+    t.right(r.choice(angles))
+
+# end
+t.exitonclick()
