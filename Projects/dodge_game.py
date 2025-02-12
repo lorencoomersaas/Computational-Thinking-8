227b721b46f130324da273caf77c840d7b309673
lorencoomersaas <lorencoomer@seattleacademy.org>
2025-02-12 17:07:51 +0000

    ee

--- Projects/dodge_game.py
+++ Projects/dodge_game.py
@@ -1,17 +1,27 @@
-# setup/importing librairies
+# setup/importing libraries
 import codesters, random
 from codesters import StageClass
 stage = StageClass()
-stage.disable_floor()
-stage.set_background("purpbg")
-ship = codesters.sprite("ship")
-ball = codesters.sprite("ball")
+stage.disable_all_walls()
+stage.set_background('purpbg')
 
 # setup ship
-ship.goto(0,-250)
+ship = codesters.Sprite('ship')
+ship.goto(0,-200)
+ship.set_size(2)
 
+game_over = False
 
-# defining functions
-def ballfall(n):
-    for i in range(n):
-        pass
+# defining functions and ball setup
+def ballfall():
+    global object_speed
+
+    if game_over == False:
+        ball = codesters.Sprite('ball')
+        ball.set_size(random.randint(2,3))
+        ball.set_y_speed(random.randint(2,3) * -1)
+
+def collision(ship, ball):
+    pass
+
+ballfall()
